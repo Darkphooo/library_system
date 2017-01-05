b14ce3f645334b38ac591e35a22644172974da55
darkphooo <dark.xiaoyao@qq.com>
2017-01-05 10:59:53 +0800

    add navbar

--- src/components/nav-home.vue
+++ src/components/nav-home.vue
@@ -9,43 +9,46 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">Brand</a>
+        <a class="navbar-brand" href="#">图书管理系统</a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Link</a></li>
-          <li><a href="#">Link</a></li>
+          <li class="active"><a href="#">首页</a></li>
+          <li><a href="#">图书查询</a></li>
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">分类浏览<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
+              <li><a href="#">小说</a></li>
             </ul>
           </li>
+          <li><a href="#" v-if="(islogin === true)">图书借阅</a></li>
+          <li><a href="#" v-if="(islogin === true)">图书归还</a></li>
         </ul>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Link</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+          <li><a href="#">注册</a></li>
+          <li class="dropdown" v-if="(islogin === false)">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">登录<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
+              <form class="navbar-form navbar-left" role="login">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="用户名">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="密码">
+                </div>
+                <button type="submit" class="btn btn-success btn-block">登录</button>
+              </form>
+            </ul>
+          </li>
+          <li class="dropdown" v-if="(islogin === true)">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{username}}<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">借阅记录</a></li>
+              <form class="navbar-form" role="logout">
+                <button type="submit" class="btn btn-danger btn-block">注销</button>
+              </form>
             </ul>
           </li>
         </ul>
@@ -55,12 +58,18 @@
 </template>
 
 <style lang="less">
+  .form-control {
+    margin-bottom: 5px;
+  }
 </style>
 
 <script>
 export default {
   data() {
-    return{}
+    return{
+      islogin: false,
+      username: '测试用户'
+    }
   },
   ready: function () {
     $('li.dropdown').mouseover(function() {   
